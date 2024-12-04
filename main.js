db31b43bed71126a8fff67c70ecc67aeac57405f
LonerLykos <loner.lykos.project@gmail.com>
2024-12-05 00:13:28 +0200

    reworked the error mechanism

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 let pairs = JSON.parse(localStorage.getItem('pairs')) || [];
 let selectedItems = [];
-let addPair = document.getElementById('addPair');
-let pairsList = document.getElementById('pairs-list');
-let deleteAll = document.getElementById('deleteAll');
+const addPair = document.getElementById('addPair');
+const pairsList = document.getElementById('pairs-list');
+const deleteAll = document.getElementById('deleteAll');
+const errorWindow = document.getElementById('errorWindow');
+const errorMsg = document.getElementById('errorMsg');
 
 if (pairs.length > 0) {
     pairs.forEach((element) => {
@@ -18,24 +20,43 @@ addPair.addEventListener('click', (e) => {
     const input = document.getElementById('nameValuePair');
     let trim = input.value.trim();
     if (trim.includes('=')) {
-        let arrPair = trim.split('=');
-        let name = arrPair[0].trim();
-        let value = arrPair[1].trim();
-        const match1 = name.match(/([a-zA-Z0-9а-яА-ЯїЇєЄіІґҐ]+)*\s?/g);
-        const match2 = value.match(/([a-zA-Z0-9а-яА-ЯїЇєЄіІґҐ]+)*\s?/g);
-        if (match1 && match2) {
-            pairs.push([name, value]);
-            localStorage.setItem('pairs', JSON.stringify(pairs));
-            input.value = '';
-            const p = document.createElement('p');
-            p.classList.add('pair');
-            p.textContent = `${name}=${value}`;
-            pairsList.appendChild(p);
+        let index = trim.indexOf('=');
+        let indexlast = trim.lastIndexOf('=');
+        if (index === indexlast) {
+            let arrPair = trim.split('=');
+            let name = arrPair[0].trim();
+            let value = arrPair[1].trim();
+            if (!name || !value) {
+                let text = `Ім'я і значення не можуть бути пустими`;
+                forError(text);
+                input.value = '';
+            } else {
+                const match1 = name.match(/^(([a-zA-Z0-9а-яА-ЯїЇєЄіІґҐ]+)*\s?)+$/);
+                const match2 = value.match(/^(([a-zA-Z0-9а-яА-ЯїЇєЄіІґҐ]+)*\s?)+$/);
+                if (match1 && match2) {
+                    pairs.push([name, value]);
+                    localStorage.setItem('pairs', JSON.stringify(pairs));
+                    input.value = '';
+                    const p = document.createElement('p');
+                    p.classList.add('pair');
+                    p.textContent = `${name}=${value}`;
+                    pairsList.appendChild(p);
+                } else {
+                    let text = `Не використовуйте символи крім '=' та пробілу`;
+                    forError(text);
+                    input.value = '';
+                }
+            }
+
         } else {
-            alert(`Невірний формат. Будь ласка, використовуйте формат Ім'я=Значення.`);
+            let text = `Не використовуйте '=' більше ніж одне на пару`;
+            forError(text);
+            input.value = '';
         }
+
     } else {
-        alert(`Використовуйте '=' для визначення пари Ім'я=Значення`);
+        let text = `Використовуйте '=' для визначення пари`;
+        forError(text);
         input.value = '';
     }
 });
@@ -43,14 +64,12 @@ addPair.addEventListener('click', (e) => {
 const sortByName = document.getElementById('sortByName');
 sortByName.addEventListener('click', (e) => {
     e.preventDefault();
-    pairsList.textContent = '';
     sortButton(0);
 });
 
 const sortByValue = document.getElementById('sortByValue');
 sortByValue.addEventListener('click', (e) => {
     e.preventDefault();
-    pairsList.textContent = '';
     sortButton(1);
 });
 
@@ -78,7 +97,8 @@ deleteSelected.addEventListener('click', (e) => {
     selectedItems = [];
 });
 
-function sortButton(index){
+function sortButton(index) {
+    pairsList.textContent = '';
     let sortItems = pairs.sort((a, b) => a[index].localeCompare(b[index]));
     sortItems.forEach((item) => {
         const p = document.createElement('p');
@@ -98,7 +118,15 @@ deleteAll.addEventListener('click', (e) => {
     selectedItems = [];
 })
 
+function forError(text) {
+    errorMsg.textContent = '';
+    errorMsg.textContent = text;
+    errorWindow.classList.add('view');
+    setTimeout(() => {
+        errorWindow.classList.remove('view');
+    }, 5000);
 
+}
 
 
 
